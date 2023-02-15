a47889f1acfb9297107f983a5ff821e418e2b610
tiantiaf <tiantiaf@usc.edu>
2023-02-15 01:36:01 -0800

    make it runnable

--- README.md
+++ README.md
@@ -1,1 +1,60 @@
-# trust-ser
+# Trustworthy Speech Emotion Recognition
+#### Trust-SER is an open source project for researchers exploring SER applications with trustworthiness elements
+
+The core elements for Trustworthy Speech Emotion Recognition:
+
+<div align="center">
+ <img src="img/trustworthy-ser.png" width="750px">
+</div>
+
+To begin with, please clone this repo:
+```
+git clone git@github.com:usc-sail/trust-ser.git
+```
+
+To install the conda environment:
+```
+cd trust-ser
+conda env create -f trust-ser.yml
+conda activate trust-ser
+```
+
+Please specify the data file to your work dir under config/config.yml
+
+```
+data_dir:
+  crema_d: CREMA_D_PATH
+  iemocap: IEMOCAP_PATH
+  meld: MELD_PATH
+  msp-improv: MSP-IMPROV_PATH
+  msp-podcast: MSP-PODCAST_PATH
+  ravdess: RAVDESS_PATH
+project_dir: OUTPUT_PATH
+```
+
+### Data Spliting
+For most of the dataset, user need first split the train/dev/test by the given script file. Take the IEMOCAP data as instance:
+```
+cd train_split_gen
+python3 iemocap.py
+```
+
+### Audio Preprocess
+For most of the dataset, user can generate the preprocessed audio file by the given script file. The preprocessing includes resample to 16kHz and to mono channel. Take the IEMOCAP data as instance:
+```
+cd preprocess_audio
+python3 preprocess_audio.py --dataset iemocap
+# dataset: iemocap, ravdess, msp-improv, msp-podcast, crema_d
+```
+
+### ML training
+To train with a pretrained backbone, use the following:
+```
+cd experiment
+CUDA_VISIBLE_DEVICES=0 taskset -c 1-30 python3 finetune_single_thread.py --pretrain_model apc --dataset iemocap --learning_rate 0.0005 --downstream_model cnn --num_epochs 30 --num_layers 3 --conv_layers 2 --pooling mean --hidden_size 128
+
+# pretrain_model: apc, tera, wavlm, wav2vec2_0, whisper (to add)
+# pooling: mean, att (self-attention)
+# hidden_size: size of cnn
+# conv_layers: number of cnn layers
+```
--- preprocess_audio/preprocess_audio.py
+++ preprocess_audio/preprocess_audio.py
@@ -7,7 +7,6 @@ import pickle, pdb, re
 
 from tqdm import tqdm
 from pathlib import Path
-from moviepy.editor import *
 
 # define logging console
 import logging
@@ -21,9 +20,23 @@ logging.basicConfig(
 if __name__ == '__main__':
 
     # data path
-    split_path = Path('/media/data/projects/speech-privacy/trust-ser/train_split')
-    audio_path = Path('/media/data/projects/speech-privacy/trust-ser/audio')
+    with open("../config/config.yml", "r") as stream:
+         config = yaml.safe_load(stream)
+    data_path   = Path(config["data_dir"]["crema_d"])
+    split_path = Path(config["project_dir"]).joinpath("train_split")
+    audio_path = Path(config["project_dir"]).joinpath("audio")
 
+    # parser
+    parser = argparse.ArgumentParser(description='input dataset')
+    parser.add_argument(
+        '--dataset', 
+        default='iemocap',
+        type=str, 
+        help='dataset name'
+    )
+    args = parser.parse_args()
+
+    # args.dataset
     # read splits
     # for dataset in ['iemocap']:
     # for dataset in ['msp-improv']:
@@ -32,48 +45,42 @@ if __name__ == '__main__':
     # for dataset in ['ravdess']:
     # for dataset in ['emov_db']:
     # for dataset in ['vox-movie']:
-    for dataset in ['msp-podcast']:
+    # for dataset in ['msp-podcast']:
     # for dataset in ['cmu-mosei']:
-        if dataset in ["iemocap", "crema_d", "ravdess", "msp-improv"]:
-            with open(str(split_path.joinpath(f'{dataset}_fold1.json')), "r") as f: split_dict = json.load(f)
-        else:
-            with open(str(split_path.joinpath(f'{dataset}.json')), "r") as f: split_dict = json.load(f)
-        for split in ['train', 'dev', 'test']:
-            Path.mkdir(audio_path.joinpath(dataset), parents=True, exist_ok=True)
-            # for idx, data in tqdm(enumerate(), ncols=10, miniters=10):
-            for idx in tqdm(range(len(split_dict[split]))):
-                data = split_dict[split][idx]
-                file_path = data[3]
-                speaker_id = data[1]
-                # if dataset in ['iemocap', 'msp-improv', 'meld', 'crema_d', 'ravdess', 'emov_db']:
-                waveform, sample_rate = torchaudio.load(str(file_path))
-                
-                if waveform.shape[0] != 1:
-                    waveform = torch.mean(waveform, dim=0).unsqueeze(0)
-                if sample_rate != 16000:
-                    transform_model = torchaudio.transforms.Resample(sample_rate, 16000)
-                    waveform = transform_model(waveform)
-                if dataset == 'cmu-mosei':
-                    start, end = int(data[4][0] * 16000), int(data[4][1] * 16000)
-                    waveform = waveform[:, start:end]
-
-                if dataset in ['iemocap', 'msp-improv', 'meld', 'crema_d', 'msp-podcast']:
-                    output_path = audio_path.joinpath(dataset, file_path.split('/')[-1])
-                elif dataset in ['ravdess', 'emov_db', 'vox-movie']:
-                    output_path = audio_path.joinpath(dataset, f'{speaker_id}_{file_path.split("/")[-1]}')
-                elif dataset in ['cmu-mosei']:
-                    output_path = audio_path.joinpath(dataset, f'{str(start).replace(".", "_")}_{file_path.split("/")[-1]}')
+    if args.dataset in ["iemocap", "crema_d", "ravdess", "msp-improv"]:
+        with open(str(split_path.joinpath(f'{args.dataset}_fold1.json')), "r") as f: split_dict = json.load(f)
+    else:
+        with open(str(split_path.joinpath(f'{args.dataset}.json')), "r") as f: split_dict = json.load(f)
+    for split in ['train', 'dev', 'test']:
+        Path.mkdir(audio_path.joinpath(args.dataset), parents=True, exist_ok=True)
+        # for idx, data in tqdm(enumerate(), ncols=10, miniters=10):
+        for idx in tqdm(range(len(split_dict[split]))):
+            data = split_dict[split][idx]
+            file_path = data[3]
+            speaker_id = data[1]
+            # if dataset in ['iemocap', 'msp-improv', 'meld', 'crema_d', 'ravdess', 'emov_db']:
+            waveform, sample_rate = torchaudio.load(str(file_path))
             
-                torchaudio.save(str(output_path), waveform, 16000)
-                split_dict[split][idx][3] = str(output_path)
-                    
-            logging.info(f'-------------------------------------------------------')
-            logging.info(f'Preprocess audio for {dataset} dataset')
-            for split in ['train', 'dev', 'test']: logging.info(f'Split {split}: Number of files {len(split_dict[split])}')
-            logging.info(f'-------------------------------------------------------')
+            if waveform.shape[0] != 1:
+                waveform = torch.mean(waveform, dim=0).unsqueeze(0)
+            if sample_rate != 16000:
+                transform_model = torchaudio.transforms.Resample(sample_rate, 16000)
+                waveform = transform_model(waveform)
+            if args.dataset == 'cmu-mosei':
+                start, end = int(data[4][0] * 16000), int(data[4][1] * 16000)
+                waveform = waveform[:, start:end]
 
-            # dump the dictionary
-            # jsonString = json.dumps(split_dict, indent=4)
-            # jsonFile = open(str(audio_path.joinpath(f'{dataset}_fold{fold_idx}.json')), "w")
-            # jsonFile.write(jsonString)
-            # jsonFile.close()
+            if args.dataset in ['iemocap', 'msp-improv', 'meld', 'crema_d', 'msp-podcast']:
+                output_path = audio_path.joinpath(args.dataset, file_path.split('/')[-1])
+            elif args.dataset in ['ravdess', 'emov_db', 'vox-movie']:
+                output_path = audio_path.joinpath(args.dataset, f'{speaker_id}_{file_path.split("/")[-1]}')
+            elif args.dataset in ['cmu-mosei']:
+                output_path = audio_path.joinpath(args.dataset, f'{str(start).replace(".", "_")}_{file_path.split("/")[-1]}')
+        
+            torchaudio.save(str(output_path), waveform, 16000)
+            split_dict[split][idx][3] = str(output_path)
+                
+        logging.info(f'-------------------------------------------------------')
+        logging.info(f'Preprocess audio for {dataset} dataset')
+        for split in ['train', 'dev', 'test']: logging.info(f'Split {split}: Number of files {len(split_dict[split])}')
+        logging.info(f'-------------------------------------------------------')
