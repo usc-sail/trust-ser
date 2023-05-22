b61c0a9d52f7c556b4181897ce429d7c0a39918e
tiantiaf@usc.edu <tiantiaf@usc.edu>
2023-05-21 20:49:35 -0700

    add uncommit code

---
+++ trustworthy/sustainability/sustainability.sh
@@ -0,0 +1,18 @@
+# CUDA_VISIBLE_DEVICES=1, taskset -c 90-120 python3 fairness_evaluation.py --pretrain_model apc --dataset msp-improv --learning_rate 0.0005 --downstream_model cnn --num_epochs 30 --num_layers 3 --conv_layers 2 --pooling mean --hidden_size 128
+# CUDA_VISIBLE_DEVICES=1, taskset -c 90-120 python3 fairness_evaluation.py --pretrain_model whisper_base --dataset msp-improv --learning_rate 0.0005 --downstream_model cnn --num_epochs 30 --num_layers 3 --conv_layers 2 --pooling mean --hidden_size 128
+# CUDA_VISIBLE_DEVICES=1, taskset -c 90-120 python3 fairness_evaluation.py --pretrain_model whisper_tiny --dataset msp-improv --learning_rate 0.0005 --downstream_model cnn --num_epochs 30 --num_layers 3 --conv_layers 2 --pooling mean --hidden_size 128
+# CUDA_VISIBLE_DEVICES=1, taskset -c 90-120 python3 fairness_evaluation.py --pretrain_model whisper_small --dataset msp-improv --learning_rate 0.0005 --downstream_model cnn --num_epochs 30 --num_layers 3 --conv_layers 2 --pooling mean --hidden_size 128
+# CUDA_VISIBLE_DEVICES=1, taskset -c 90-120 python3 fairness_evaluation.py --pretrain_model wav2vec2_0 --dataset msp-improv --learning_rate 0.0005 --downstream_model cnn --num_epochs 30 --num_layers 3 --conv_layers 2 --pooling mean --hidden_size 128
+# CUDA_VISIBLE_DEVICES=1, taskset -c 90-120 python3 fairness_evaluation.py --pretrain_model tera --dataset msp-improv --learning_rate 0.0005 --downstream_model cnn --num_epochs 30 --num_layers 3 --conv_layers 2 --pooling mean --hidden_size 128
+# CUDA_VISIBLE_DEVICES=1, taskset -c 90-120 python3 fairness_evaluation.py --pretrain_model wavlm --dataset msp-improv --learning_rate 0.0005 --downstream_model cnn --num_epochs 30 --num_layers 3 --conv_layers 2 --pooling mean --hidden_size 128
+
+for pretrain_model in apc whisper_base whisper_tiny whisper_small wav2vec2_0 tera wavlm; do
+# for pretrain_model in wavlm; do
+# for pretrain_model in wav2vec2_0; do
+# for pretrain_model in whisper_base whisper_tiny whisper_small; do
+# for pretrain_model in apc; do
+    # for dataset in iemocap crema_d msp-improv msp-podcast; do
+    for dataset in iemocap; do
+        CUDA_VISIBLE_DEVICES=0, taskset -c 90-120 python3 sustainability.py --pretrain_model $pretrain_model --dataset $dataset --learning_rate 0.0005 --downstream_model cnn --num_epochs 30 --num_layers 3 --conv_layers 2 --pooling mean --hidden_size 128
+    done
+done
